ded1c232118beed67299515e6caa737daa464f52
Magnolia <biamarschneider@gmail.com>
2025-06-04 01:40:16 -0300

    Projeto Maven tic-tac-toe criado

--- tic-tac-toe/src/main/java/ticTacToe/component/Button.java
+++ tic-tac-toe/src/main/java/ticTacToe/component/Button.java
@@ -1,94 +1,40 @@
 package ticTacToe.component;
 
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-import ticTacToe.gui.Paintable;
-import ticTacToe.gui.util.MouseListenerAdapter;
-
-public class Button implements Paintable {
-
-	private Point position = null;
-	private Dimension dimension = null;
+public class Button extends AbstractComponent {
 
 	public Button() {
-		this.position = new Point(0, 0);
-		this.dimension = new Dimension(20, 20);
+		super();
 	}
 
 	public Button(int x, int y) {
-		this();
-		this.setPosition(x, y);
+		super(x, y);
 	}
 
 	public Button(int x, int y, int width, int height) {
-		this(x, y);
-		this.setSize(width, height);
+		super(x, y, width, height);
 	}
 
-	public void setPosition(int x, int y) {
-		this.position = new Point(x, y);
-
-	}
-
-	public Point getPosition() {
-		return new Point(this.position);
-	}
-
-	public void setSize(int width, int height) {
-		this.dimension = new Dimension(width, height);
-	}
-
-	public Dimension getSize() {
-		return new Dimension(this.dimension);
-	}
-
-	public int width() {
-		return this.dimension.width;
-	}
-
-	public int height() {
-		return this.dimension.height;
-	}
-
-	public boolean isOver(Point point) {
-		
-		int xLeft = this.position.x;
-		int yTop = this.position.y;
-		int xRight = xLeft + this.width();
-		int yBotton = yTop + this.height();
-		
-		return ( (point.x > xLeft && point.x < xRight) && (point.y > yTop && point.y < yBotton) );
-	}
-	
-	//--reacting to mouse click ---------------------------
-	
-	public MouseListener mouseListener() {
-		
-		return new MouseListenerAdapter() {
-			
-			@Override
-			public void mouseClicked( MouseEvent me) {
-				
-				if( !isOver( me.getPoint() ))
-					return;
-				
-				System.out.println("oieeee!");
-			}
-		};
-	}
-	
 	@Override
 	public void paint(Graphics g) {
-		int xLeft = this.position.x;
-		int yTop = this.position.y;
-		int width = this.width();
-		int height = this.height();
 
-		g.drawRect(xLeft, yTop, width, height);
+		g.drawRect(position.x, position.y, width(), height());
+
+		if (this.mouseOver)
+			doMouseOverDecoration(g);
+
 	}
 
+	private void doMouseOverDecoration(Graphics g) {
+
+		int offset = 5;
+		int doubleOffset = offset * 2;
+		Color buttonColor = g.getColor();
+
+		g.setColor(Color.pink);
+		g.drawRect(position.x + offset, position.y + offset, width() - doubleOffset, height() - doubleOffset);
+		g.setColor(buttonColor);
+	}
 }
--- tic-tac-toe/src/test/java/appTest/AppButtonMouseListenerTest.java
+++ tic-tac-toe/src/test/java/appTest/AppButtonMouseListenerTest.java
@@ -3,13 +3,14 @@ package appTest;
 import ticTacToe.component.Button;
 import ticTacToe.gui.MainWindow;
 
-public class AppButtonMouseClickTest {
-	
+public class AppButtonMouseListenerTest {
+
 	public static void main(String[] args) {
-		
+
 		MainWindow window = new MainWindow();
 		Button button = new Button(250, 100, 50, 50);
-		window.add(button);;
+		window.add(button);
 		window.addMouseListener(button.mouseListener());
+		window.addMouseMotionListener(button.mouseMotionListener());
 	}
 }
